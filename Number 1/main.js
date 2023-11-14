27894c56954caf591ab8c073f1cac1343644f7ee
Doris Ikeme <110048538+DorisDecency@users.noreply.github.com>
2023-11-14 14:42:41 -0800

    .

--- Number 1/main.js
+++
@@ -1,6 +0,0 @@
-function change() {
-    let message = document.getElementById("para-change");
-    var newMessage = document.createElement('newMessage');
-    newMessage.innerHTML = "Finally, you clicked the button";
-    message.appendChild(newMessage);
-}
